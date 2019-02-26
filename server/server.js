f8d6f53253350c7fffaf99f40f764e86773266ea
MacBookPro <macbookpro@MacBookPro.local>
2019-02-26 18:20:54 +0100

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -1,12 +1,50 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io')
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
-app.listen(port, ()=> {
+
+//CONNECT
+io.on('connection', (socket)=> {
+ console.log('new user connected');
+
+
+
+// NEW STANDARD USER MESSAGE RECEIVED 
+socket.on('createStandardMessage', (receivedFromClient) =>{
+    console.log('new standard user message', receivedFromClient)
+    //EMIT TO ALL CONNECTIONS
+    io.emit('standardMessage', {
+        from: receivedFromClient.from,
+        text: receivedFromClient.text,
+        createdAt: new Date().getTime()
+    });
+});
+
+
+//NEW STANDARD USER MESSAGE REDIRECTED
+// socket.emit('standardMessage', {
+//     from: 'Mike@example.com',
+//     text: 'Hey. What is going on',
+//     createdAt: 123
+// });
+
+// DISCONNECT
+ socket.on('disconnect', () => {
+ console.log('user was disconnected');
+ });
+
+});
+
+
+server.listen(port, ()=> {
     console.log(`Server is up on ${port}`)
 })
